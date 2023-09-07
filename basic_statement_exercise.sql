8ad9b1e3921f58aeca13e9f5151e903e077bacac
Nikki de Vries <nikki.devries7@gmail.com>
2023-09-07 10:27:32 -0500

    Final Practice for basic data sceince

--- basic_statement_exercise.sql
+++ basic_statement_exercise.sql
@@ -1,40 +1,64 @@
--- Use the albums_db database.
+-- 1. Use the albums_db database.
 SHOW DATABASES;
 USE albums_db;
 SELECT DATABASE();
 SHOW Tables;
 DESCRIBE albums;
 
--- What is the primary key for the albums table?
-#The Primary Key is id 
+-- 2. What is the primary key for the albums table?
+DESCRIBE albums;
+#The Primary Key is id column
 
--- What does the column named 'name' represent?
-#The name of the song
+-- 3. What does the column named 'name' represent?
+#The name of an album
 
--- What do you think the sales column represents?
-# The number of sales the song has done (How much money it has brought in)
+-- 4. What do you think the sales column represents?
+# The number of album sales (How much moeney it has brought in - Millions)
 
--- Find the name of all albums by Pink Floyd.
-SELECT artist, name from albums
+-- 5. Find the name of all albums by Pink Floyd.
+SELECT artist, name 
+FROM albums
 WHERE artist = 'Pink Floyd';
 # The two songs are The Dark Side of the Moon and The Wall
 	
 
--- What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
-SELECT name, release_date from albums
+-- 6. What is the year Sgt. Pepper's Lonely Hearts Club Band was released?
+SELECT 
+	name, 
+    release_date 
+FROM albums
 WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band'; 
 # The release_date was 1967
 
--- What is the genre for the album Nevermind?
-SELECT artist, name, genre from albums
+-- 7. What is the genre for the album Nevermind?
+SELECT artist, name, genre 
+FROM albums
 WHERE name = 'Nevermind';
 # The genre is grunge, alternative rock
 
--- Which albums were released in the 1990s?
-SELECT name, release_date from albums
-WHERE release_date = 1990;
-# the album released in 1990 is The Immaculate Collection
+-- 8. Which albums were released in the 1990s?
+SELECT name, release_date
+FROM albums
+WHERE release_date between 1990 and 1999;
 
--- Which albums had less than 20 million certified sales? Rename this column as low_selling_albums.
-SELECT name as low_selling_albums from albums
-WHERE sales < 20;
+/* There are 11 albums:
+The bodygaurd
+Jagged Little Pill
+Come on Over
+Falling into You
+Let's talk about love
+dangerous
+the immaculate collection
+Titanic: music from the motion picture
+Metallica
+nevermind
+supernatural
+*/
+
+
+-- 9. Which albums had less than 20 million certified sales? Rename this column as low_selling_albums.
+SELECT 
+	name as low_selling_albums,
+    sales    
+FROM albums
+WHERE sales < 20.0;
